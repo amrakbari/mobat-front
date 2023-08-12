0830eb2d6fc1db60ecc9d88b25d015e2cd1f02ca
HeliaGharaeian <gharaeianhelia@gmail.com>
2023-08-12 23:48:25 +0330

    new services and interface added

---
+++ src/app/detail.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {UserDataInterface} from "./user-data-interface";
+import {enviroment} from "../../environments/environments";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetailService {
+
+  constructor(private http: HttpClient) { }
+
+  getStoreById(id: string): Observable<any> {
+    let finalData: UserDataInterface;
+    const token = localStorage.getItem('access_token');
+    return this.http.get(`${enviroment.apiurl}/store/stores/${id}/`, {headers: {'Authorization': `JWT ${token}`}});
+  }
+
+  getAddressById(id: number): Observable<any> {
+    let finalData: UserDataInterface;
+    const token = localStorage.getItem('access_token');
+    return this.http.get(`${enviroment.apiurl}/accounts/addresses/${id}/`, {headers: {'Authorization': `JWT ${token}`}});
+  }
+
+}
